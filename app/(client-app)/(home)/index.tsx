4b2aecd54fe062fb006edcfa220b8d4d795ba88a
alexsandro2005 <luisalejandrm533@gmail.com>
2025-01-11 22:04:53 -0500

    feat: add config fonts Kanit/ config color primary and secondaries

--- app/(client-app)/(home)/index.tsx
+++ app/(client-app)/(home)/index.tsx
@@ -1,10 +1,16 @@
 import { ThemedText } from "@/presentation/theme/components/ThemedText";
+import { useThemeColor } from "@/presentation/theme/hooks/useThemeColor";
 import { View } from "react-native";
 
 const HomeScreen = () => {
+  const primary = useThemeColor({}, "primary");
   return (
-    <View>
-      <ThemedText>HomeScreen</ThemedText>
+    <View style={{ padding: 100 }}>
+      <ThemedText style={{ fontFamily: "KanitBold", color: primary }}>
+        HomeScreen
+      </ThemedText>
+      <ThemedText style={{ fontFamily: "KanitRegular" }}>HomeScreen</ThemedText>
+      <ThemedText style={{ fontFamily: "KanitThin" }}>HomeScreen</ThemedText>
     </View>
   );
 };
--- constants/Colors.ts
+++ constants/Colors.ts
@@ -1,26 +1,27 @@
-/**
- * Below are the colors that are used in the app. The colors are defined in the light and dark mode.
- * There are many other ways to style your app. For example, [Nativewind](https://www.nativewind.dev/), [Tamagui](https://tamagui.dev/), [unistyles](https://reactnativeunistyles.vercel.app), etc.
- */
-
-const tintColorLight = '#0a7ea4';
-const tintColorDark = '#fff';
+const tintColorLight = "#0a7ea4";
+const tintColorDark = "#fff";
 
 export const Colors = {
   light: {
-    text: '#11181C',
-    background: '#fff',
+    text: "#043201",
+    background: "#f1feef",
     tint: tintColorLight,
-    icon: '#687076',
-    tabIconDefault: '#687076',
+    icon: "#687076",
+    tabIconDefault: "#687076",
     tabIconSelected: tintColorLight,
+    primary: "#24ca12",
+    secondaryLight: "#19b009",
+    terciaryLight: "#178a0b",
   },
   dark: {
-    text: '#ECEDEE',
-    background: '#151718',
+    text: "#f1feef",
+    background: "#043201",
     tint: tintColorDark,
-    icon: '#9BA1A6',
-    tabIconDefault: '#9BA1A6',
+    icon: "#9BA1A6",
+    tabIconDefault: "#9BA1A6",
     tabIconSelected: tintColorDark,
+    primary: "#24ca12",
+    secondaryDark: "#4eec3c",
+    terciaryDark: "#89f97c",
   },
 };
