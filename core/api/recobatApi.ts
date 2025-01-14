d52787ed6e71d906cd142511096323f9ea7d0b5d
alexsandro2005 <luisalejandrm533@gmail.com>
2025-01-13 21:14:09 -0500

    feat: add config OS (IOS or Android)

--- core/api/recobatApi.ts
+++ core/api/recobatApi.ts
@@ -1,9 +1,21 @@
-//TODO: conectar mediante vars, Android e IOS
 //* importamos axios
 import axios from "axios";
+import { Platform } from "react-native";
+
+//TODO: conectar mediante vars, Android e IOS
+const STAGE = process.env.EXPO_PUBLIC_STAGE || "dev";
+
+export const API_URL =
+  STAGE === "production"
+    ? process.env.EXPO_PUBLIC_API_URL
+    : Platform.OS === "ios"
+    ? process.env.EXPO_PUBLIC_API_URL_IOS
+    : process.env.EXPO_PUBLIC_API_URL_ANDROID;
+
+console.log({ STAGE, [Platform.OS]: API_URL });
 
 const recobatApi = axios.create({
-  baseURL: "localhost:1337/api",
+  baseURL: API_URL,
 });
 
 //TODO: implementar los interceptores
