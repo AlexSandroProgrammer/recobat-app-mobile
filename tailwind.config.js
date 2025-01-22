bf58308759b345557628617d11128471865f7d7a
alexsandro2005 <luisalejandrm533@gmail.com>
2025-01-21 23:37:52 -0500

    feat: add config tailwind and image type declare

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,22 @@ module.exports = {
   ],
   presets: [require("nativewind/preset")],
   theme: {
-    extend: {},
+    extend: {
+      // agregamos la fuente Kanit y los colores de la paleta
+      fontFamily: {
+        kanit: ["Kanit-Regular", "sans-serif"],
+        "kanit-thin": ["Kanit-Thin", "sans-serif"],
+        "kanit-bold": ["Kanit-Bold", "sans-serif"],
+      },
+      colors: {
+        primary: {
+          100: "#f0f6fe",
+          200: "#4880e9",
+          300: "#273d88",
+          400: "#1b2550",
+        },
+      },
+    },
   },
   plugins: [],
 };
