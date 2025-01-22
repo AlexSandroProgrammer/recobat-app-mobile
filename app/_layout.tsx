8f22805cbead661e348277de45d9f883f4a02030
alexsandro2005 <luisalejandrm533@gmail.com>
2025-01-21 23:25:23 -0500

    feat: add fonts and assets

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,24 @@
-import { Stack } from "expo-router";
+import { SplashScreen, Stack } from "expo-router";
 import "../global.css";
+import { useFonts } from "expo-font";
+import { useEffect } from "react";
 
 export default function RootLayout() {
+  // realizamos la importacion de los tipos de letra
+  const [fontsLoaded] = useFonts({
+    "Kanit-Bold": require("../assets/fonts/Kanit-Bold.ttf"),
+    "Kanit-Regular": require("../assets/fonts/Kanit-Regular.ttf"),
+    "Kanit-Thin": require("../assets/fonts/Kanit-Thin.ttf"),
+  });
+
+  useEffect(() => {
+    if (fontsLoaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  // si falla la carga de las fuentes
+  if (!fontsLoaded) return null;
+
   return <Stack />;
 }
