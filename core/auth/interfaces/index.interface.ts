0fcaa6f1dff5a06b0293db710bc144c7a611f3ca
alexsandro2005 <luisalejandrm533@gmail.com>
2025-01-22 19:56:42 -0500

    feat: add auth actions/ add client actions/ update custom hook useAuthStore

---
+++ core/auth/interfaces/index.interface.ts
@@ -0,0 +1,32 @@
+// interfaz con los datos del usuario
+export interface UserData {
+  id: number;
+  documentId: string;
+  username: string;
+  email: string;
+  provider: string;
+  confirmed: boolean;
+  blocked: boolean;
+  createdAt: Date;
+  updatedAt: Date;
+  publishedAt: Date;
+  names: string;
+  surnames: string;
+  document: string;
+  telephone: string;
+  stateData: string;
+}
+
+// interfaz con los datos iniciales del usuario
+//* creamos el interface que tendra los datos del usuario
+export interface User {
+  id: number;
+  documentId: string;
+  email: string;
+  names: string;
+  surnames: string;
+  document: string;
+  username: string;
+  blocked?: boolean;
+  confirmed?: boolean;
+}
